7c6cd8381e7829a0b9e14b323553c4b72064c88f
SydlWang <sydney.l.wang@duke.edu>
2023-04-02 15:19:10 +0200

    controller updated

--- Assets/Scripts/Character/FPSInputController.cs
+++ Assets/Scripts/Character/FPSInputController.cs
@@ -22,19 +22,14 @@ public class FPSInputController : MonoBehaviour
 
 		if (directionVector != Vector3.zero)
 		{
-			// Get the length of the directon vector and then normalize it
-			// Dividing by the length is cheaper than normalizing when we already have the length anyway
 			float directionLength = directionVector.magnitude;
 			directionVector = directionVector / directionLength;
 
 			// Make sure the length is no bigger than 1
 			directionLength = Mathf.Min(1, directionLength);
 
-			// Make the input vector more sensitive towards the extremes and less sensitive in the middle
-			// This makes it easier to control slow speeds when using analog sticks
 			directionLength = directionLength * directionLength;
 
-			// Multiply the normalized direction vector by the modified length
 			directionVector = directionVector * directionLength;
 		}
 
