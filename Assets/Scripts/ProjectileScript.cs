1257877a9188343146830b08e2892c598332f651
sastahl28 <112001400+sastahl28@users.noreply.github.com>
2023-04-14 11:58:11 +0200

    Ball destroy on collisions

--- Assets/Scripts/ProjectileScript.cs
+++ Assets/Scripts/ProjectileScript.cs
@@ -4,13 +4,11 @@ using UnityEngine;
 
 public class ProjectileScript : MonoBehaviour
 {
-    public Rigidbody rb;
+
     public GameObject ball;
 
-    void Start()
-    {
-        rb = GetComponent<Rigidbody>();   
-    }
+
+
 
 
     void OnCollisionEnter(Collision collision)
