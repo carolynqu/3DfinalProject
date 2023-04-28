51366b0206101ef87517614c130565a270798416
SydlWang <sydney.l.wang@duke.edu>
2023-04-28 13:03:58 +0200

    color switcher done

--- Assets/Scripts/ColorSwitcher.cs
+++ Assets/Scripts/ColorSwitcher.cs
@@ -6,44 +6,87 @@ using UnityEngine.UI;
 public class ColorSwitcher : MonoBehaviour
 {
     public GameObject targetObject;
+    private Renderer obj;
+    //black = new Color(0, 0, 0);
+    //blue = new Color(5, 172, 255);
+    //green = new Color(67, 255, 38);
+    //orange = new Color(255, 149, 0);
+    //red = new Color(255, 38, 38);
+    //white = new Color (255, 255, 255);
+    //yellow = new Color (255, 210, 31);
+    public static bool blue = false;
+    public static bool black = false;
+    public static bool green = false;
+    public static bool orange = false;
+    public static bool red = false;
+    public static bool white = false;
+    public static bool yellow = false;
 
-    private Color[] colors = { Color.red, Color.blue, Color.green };
+    public Material [] colors;
+
     private int currentColorIndex = 0;
-
+    private Material currentColor;
+    private void Start()
+    {
+        obj = targetObject.GetComponent<Renderer>();
+        currentColor = colors[0];
+    }
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
-            SwitchColor(0);
+            currentColor = colors[0];
+            ResetBool();
+            black = true;
         }
         if (Input.GetKeyDown(KeyCode.Alpha2))
         {
-            SwitchColor(1);
+            currentColor = colors[1];
+            ResetBool();
+            blue = true;
         }
         if (Input.GetKeyDown(KeyCode.Alpha3))
         {
-            SwitchColor(2);
+            currentColor = colors[2];
+            ResetBool();
+            green = true;
         }
         if (Input.GetKeyDown(KeyCode.Alpha4))
         {
-            SwitchColor(3);
+            currentColor = colors[3];
+            ResetBool();
+            orange = true;
         }
         if (Input.GetKeyDown(KeyCode.Alpha5))
         {
-            SwitchColor(4);
+            currentColor = colors[4];
+            ResetBool();
+            red = true;
         }
         if (Input.GetKeyDown(KeyCode.Alpha6))
         {
-            SwitchColor(5);
+            currentColor = colors[5];
+            ResetBool();
+            white = true;
         }
         if (Input.GetKeyDown(KeyCode.Alpha7))
         {
-            SwitchColor(6);
+            currentColor = colors[6];
+            ResetBool();
+            yellow = true;
+
         }
+        obj.material = currentColor;
     }
 
-    void SwitchColor(int index)
+    void ResetBool()
     {
-        targetObject.GetComponent<Renderer>().material.color = colors[index];
+        blue = false;
+        black = false;
+        green = false;
+        orange = false;
+        red = false;
+        white = false;
+        yellow = false;
     }
 }
