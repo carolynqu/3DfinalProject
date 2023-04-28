8fb17bb5b9afb76996494cc6ec1ab31b8a4f55df
SydlWang <sydney.l.wang@duke.edu>
2023-04-28 11:10:07 +0200

    color switcher prototype

--- Assets/Scripts/ColorSwitcher.cs
+++ Assets/Scripts/ColorSwitcher.cs
@@ -6,19 +6,44 @@ using UnityEngine.UI;
 public class ColorSwitcher : MonoBehaviour
 {
     public GameObject targetObject;
-    public Button colorSwitchButton;
 
     private Color[] colors = { Color.red, Color.blue, Color.green };
     private int currentColorIndex = 0;
 
-    void Start()
+    void Update()
     {
-        colorSwitchButton.onClick.AddListener(SwitchColor);
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            SwitchColor(0);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            SwitchColor(1);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            SwitchColor(2);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            SwitchColor(3);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha5))
+        {
+            SwitchColor(4);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha6))
+        {
+            SwitchColor(5);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha7))
+        {
+            SwitchColor(6);
+        }
     }
 
-    void SwitchColor()
+    void SwitchColor(int index)
     {
-        currentColorIndex = (currentColorIndex + 1) % colors.Length;
-        targetObject.GetComponent<Renderer>().material.color = colors[currentColorIndex];
+        targetObject.GetComponent<Renderer>().material.color = colors[index];
     }
 }
