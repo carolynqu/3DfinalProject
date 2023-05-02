ec073dcd4e22056ec8e43a2f5793e831c95767b6
carolynqu <67798872+carolynqu@users.noreply.github.com>
2023-05-02 11:34:42 +0200

    looping

--- Assets/Scripts/PowerBar.cs
+++ Assets/Scripts/PowerBar.cs
@@ -3,71 +3,38 @@ using UnityEngine.UI;
 
 public class PowerBar : MonoBehaviour
 {
-    //public SpriteRenderer[] sprites; // Array of SpriteRenderers to loop through
-
-    public Image[] images; // Array of Images to loop through
-    public Sprite[] sprites; // Array of Sprites to assign to the Images
-
+    Image image; // Reference to the Image component on the Canvas
+    public Sprite[] sprites; // Array of Sprites to loop through
     private int currentIndex = 0; // Current index of the displayed sprite
-
-    private bool isTabHeld = false; // Flag to track if Tab button is held down
 
     public float loopInterval = 0.5f; // Interval between sprite changes
     private float timer = 0f; // Timer to track the interval
 
+    private void Start()
+    {
+        // Assign the Image component from the same GameObject
+        image = GetComponent<Image>();
+    }
+
     private void Update()
     {
-        if (Input.GetKey(KeyCode.Tab))
+        timer += Time.deltaTime;
+        if (timer >= loopInterval)
         {
-            if (!isTabHeld)
-            {
-                // Tab button pressed down
-                isTabHeld = true;
-                currentIndex = 0; // Reset the index to the first sprite
-                ShowCurrentSprite();
-            }
-
-            timer += Time.deltaTime;
-            if (timer >= loopInterval)
-            {
-                // Time interval reached, move to the next sprite
-                timer = 0f;
-                currentIndex++;
-                if (currentIndex >= sprites.Length)
-                {
-                    currentIndex = 0; // Loop back to the first sprite
-                }
-                ShowCurrentSprite();
-            }
-        }
-        else
-        {
-            if (isTabHeld)
-            {
-                // Tab button released
-                isTabHeld = false;
-            }
+            // Time interval reached, move to the next sprite
             timer = 0f;
+            currentIndex++;
+            if (currentIndex >= sprites.Length)
+            {
+                currentIndex = 0; // Loop back to the first sprite
+            }
+            ShowCurrentSprite();
         }
     }
 
     private void ShowCurrentSprite()
     {
-         for (int i = 0; i < images.Length; i++)
-        {
-            images[i].sprite = sprites[currentIndex];
-        }
-        //// Hide all sprites except the current one
-        //for (int i = 0; i < sprites.Length; i++)
-        //{
-        //    if (i == currentIndex)
-        //    {
-        //        sprites[i].gameObject.SetActive(true);
-        //    }
-        //    else
-        //    {
-        //        sprites[i].gameObject.SetActive(false);
-        //    }
-        //}
+        // Update the Image component with the current sprite
+        image.sprite = sprites[currentIndex];
     }
 }
